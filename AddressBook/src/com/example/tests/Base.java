2f093fb304430ec406861bf4482dc3023465ed11
gulirina <irene.gulyaeva@mail.ru>
2014-04-11 12:48:24 +0400

    Домашнее задание № 14
    Реализована загрузку параметров из properties-файла.
    Загружаются требуемый браузер и базовый URL
    Созданы файлы
    application.properties -  из него загружаются параметры, если явно не
    задан файл с параметрами
    firefox.properties  - браузер ФФ
    chrome.properties - браузер Хром
    
    Так же созданы бат-файлы
    ff_testsuit.bat - для запуска тестов в ФФ
    chrome_testsuit.bat - для запуска тестов в хроме
    test-suit.bat не указывает никакого файла с параметрами, соответственно
    параметры берутся из application.properties, и прогон тестов
    осуществляется тоже в ФФ

--- AddressBook/src/com/example/tests/Base.java
+++ AddressBook/src/com/example/tests/Base.java
@@ -1,17 +1,19 @@
 package com.example.tests;
 
-import java.io.IOException;
+import static com.example.tests.ContactDataGenerator.generateRandomContact;
+import static com.example.tests.GroupDataGenerator.generateRandomGroups;
+
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
+import java.util.Properties;
 
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 
-import static com.example.tests.GroupDataGenerator.generateRandomGroups;
-import static com.example.tests.ContactDataGenerator.generateRandomContact;
 import com.example.fw.ApplicationManager;
 
 public class Base {
@@ -20,7 +22,10 @@ public class Base {
 
 	@BeforeTest 
 	public void setUp() throws Exception {
-		app = new ApplicationManager();
+		String configFile = System.getProperty("configFile","application.properties");
+		Properties properties = new Properties();
+		properties.load(new FileReader(new File(configFile)));		
+		app = new ApplicationManager(properties);
 	}
 		
 	@AfterTest 
